2742dda798a6752f7335b481607c92d255e27121
mjkahan <44353175+mjkahan@users.noreply.github.com>
2019-02-13 22:26:33 -0600

    Rename View.sql to YouTube_ViewQuery.sql

---
+++ YouTube_ViewQuery.sql
@@ -0,0 +1,25 @@
+﻿SELECT 
+R.ID,
+R.EmployeeID,
+R.SalesQuantity,
+R.SalesRevenue,
+R.OrderYear,
+Demo.ufnGetFirstOrderDate(R.EmployeeID) AS FirstOrderDate,
+P.FirstName,
+P.LastName,
+A.City,
+SP.StateProvinceCode,
+CR.CountryRegionCode
+
+FROM 
+Demo.RepPerformance R
+INNER JOIN Person.Person AS P 
+	ON P.BusinessEntityID = R.EmployeeID
+INNER JOIN Person.BusinessEntityAddress AS BEA 
+	ON BEA.BusinessEntityID = R.EmployeeID
+INNER JOIN Person.[Address] AS A 
+	ON A.AddressID = BEA.AddressID  
+INNER JOIN Person.StateProvince AS SP 
+	ON SP.StateProvinceID = A.StateProvinceID 
+INNER JOIN Person.CountryRegion AS CR 
+	ON CR.CountryRegionCode = SP.CountryRegionCode
